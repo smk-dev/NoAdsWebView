36ddc11395fa7db64f28157e62e370fea9f93074
smk-dev <harrymagik@gmail.com>
2013-03-27 21:24:44 +0100

    Extending filter class

--- NoAdsWebView/smkUrlFilter.h
+++ NoAdsWebView/smkUrlFilter.h
@@ -10,7 +10,17 @@
 
 @interface smkUrlFilter : NSObject
 
+// Facebook
 @property (nonatomic, assign) BOOL blockFacebookLike;
+@property (nonatomic, assign) BOOL blockFacebookLikeBox;
+@property (nonatomic, assign) BOOL blockFacebookConnect;
+
+// Google
+@property (nonatomic, assign) BOOL blockGooglePlus;
+@property (nonatomic, assign) BOOL blockGoogleAnalytics;
+
+// Twitter
+@property (nonatomic, assign) BOOL blockTwitterFolow;
 
 - (BOOL)acceptURL:(NSURL *)url;
 
--- NoAdsWebView/smkUrlFilter.m
+++ NoAdsWebView/smkUrlFilter.m
@@ -11,72 +11,116 @@
 @implementation smkUrlFilter
 
 @synthesize blockFacebookLike;
+@synthesize blockFacebookLikeBox;
+@synthesize blockFacebookConnect;
+
+#pragma mark - Public methods
 
 - (BOOL)acceptURL:(NSURL *)url {
     
     // don't load about:blank
-    if (!url.host) {
+    if ([url.absoluteString isEqualToString:@"about:blank"]) {
         return NO;
     }
     
-    // blocking facebook like button
+    // facebook like button
     if (self.blockFacebookLike && [self isFacebookLikeURL:url]) {
         LogInfo(@"Reject: %@", [self simpleUrlString:url]);
         return NO;
     }
     
-    if (self.blockFacebookLike && [self isFacebookConnectURL:url]) {
+    // facebook like box
+    if (self.blockFacebookLikeBox && [self isFacebookLikeBoxURL:url]) {
         LogInfo(@"Reject: %@", [self simpleUrlString:url]);
         return NO;
     }
     
-    if (self.blockFacebookLike && [self isFacebookLikeBoxURL:url]) {
+    // facebook connect
+    if (self.blockFacebookConnect && [self isFacebookConnectURL:url]) {
         LogInfo(@"Reject: %@", [self simpleUrlString:url]);
         return NO;
     }
     
-    if ([url.host isEqualToString:@"www.facebook.com"]) {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // twitter follow button
+    if (self.blockTwitterFolow && [self isTwitterFollowURL:url]) {
+        LogInfo(@"Reject: %@", [self simpleUrlString:url]);
         return NO;
     }
-    
     
     LogInfo(@"Accept: %@", [self simpleUrlString:url]);
     
     return YES;
 }
 
-
-#pragma mark - Private methods
+#pragma mark - Facebook
 
 - (BOOL)isFacebookLikeURL:(NSURL *)url {
-    return [url.host isEqualToString:@"www.facebook.com"]
-        && [url.relativePath isEqualToString:@"/plugins/like.php"];
+    return [url.host isEqualToString:@"www.facebook.com"] && [url.relativePath isEqualToString:@"/plugins/like.php"];
 }
 
 - (BOOL)isFacebookConnectURL:(NSURL *)url {
-    return ([url.host isEqualToString:@"static.ak.facebook.com"]
-         || [url.host isEqualToString:@"s-static.ak.facebook.com"])
-         && [url.relativePath isEqualToString:@"/connect/xd_arbiter.php"];
+    
+    if ([url.host isEqualToString:@"static.ak.facebook.com"] && [url.relativePath isEqualToString:@"/connect/xd_arbiter.php"]) {
+        return YES;
+    }
+    
+    if ([url.host isEqualToString:@"s-static.ak.facebook.com"] && [url.relativePath isEqualToString:@"/connect/xd_arbiter.php"]) {
+        return YES;
+    }
+    
+    if ([url.host isEqualToString:@"www.facebook.com"] && [url.relativePath isEqualToString:@"/dialog/oauth"]) {
+        return YES;
+    }
+    
+    if ([url.host isEqualToString:@"www.facebook.com"] && [url.relativePath isEqualToString:@"/plugins/login_button.php"]) {
+        return YES;
+    }
+    
+    return NO;
 }
 
 - (BOOL)isFacebookLikeBoxURL:(NSURL *)url {
-    return [url.host isEqualToString:@"www.facebook.com"]
-        && [url.relativePath isEqualToString:@"/plugins/likebox.php"];
+    return [url.host isEqualToString:@"www.facebook.com"] && [url.relativePath isEqualToString:@"/plugins/likebox.php"];
 }
+
+- (BOOL)isFacebookRecomendations:(NSURL *)url {
+    return [url.host isEqualToString:@"www.facebook.com"] && [url.relativePath isEqualToString:@"/plugins/recommendations.php"];
+}
+
+#pragma mark - Twitter
 
 - (BOOL)isTwitterFollowURL:(NSURL *)url {
     return [url.host isEqualToString:@"platform.twitter.com"];
 }
 
+#pragma mark - Google
+
 - (BOOL)isGoogleAnalyticsURL:(NSURL *)url {
     return [url.host isEqualToString:@"www.google-analytics.com"];
 }
 
-- (BOOL)isGooglePlusURL:(NSURL *)url {
-    return [url.host isEqualToString:@"plus.google.com"];
+- (BOOL)isGooglePlusOneURL:(NSURL *)url {
+    return [url.host isEqualToString:@"plusone.google.com"];
 }
 
+- (BOOL)isGoogleDoubleClickURL:(NSURL *)url {
+    return [url.host isEqualToString:@"googleads.g.doubleclick.net"];
+}
 
+#pragma mark - Ads
+
+
+
+#pragma mark - Private methods
 
 - (NSString *)simpleUrlString:(NSURL *)url {
     return [NSString stringWithFormat:@"%@://%@%@", url.scheme, url.host, url.relativePath];
